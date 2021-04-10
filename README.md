d4824eb8f93c7a5ec96d1934a1f84cdc004ba867
lord-gaurav-first <ashwani.rathode@gmail.com>
2021-04-10 11:17:37 +0530

    First Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Project 26
+# Cheese Sandwich Recipe by Gaurav Surelia
---
+++ sketch.js
@@ -0,0 +1,10 @@
+// var input = prompt("What is your name: ");
+
+// // if (input === undefined){
+// //     input = "Visitor";
+// // }
+
+// console.log(input);
+
+// var H1tag = document.getElementById("head");
+// H1tag.write(input);
